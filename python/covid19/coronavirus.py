46f6e7ffc4be7e6128653e4d1a9812957abfbaaa
NotCompsky <adammarkgray@gmail.com>
2021-05-05 02:39:57 +0100

    .

--- python/covid19/coronavirus.py
+++ python/covid19/coronavirus.py
@@ -2,7 +2,12 @@
 
 
 from urllib.error import HTTPError
+from datetime import datetime as dt
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
+# import matplotlib.dates as mdates
+import numpy as np
 
 
 REGIONS_DIR:str = os.path.join(os.path.dirname(os.path.realpath(__file__)), "regions")
@@ -57,14 +62,16 @@ def get_population(region:str):
 
 get_population.df = None
 
-def graph_arr(_min:int, ax, arr, colour:str, name:str, per_capita:bool, region_list:list, line_width:int, linestyle:str):
+def graph_arr(_min:int, ax, arr, colour:str, name:str, per_capita:bool, country2value:dict, df_mutable, region_list:list, line_width:int, linestyle:str):
 	n_infected_now:int = arr[-1]
 	if n_infected_now < _min:
 		return
 	arr.index = pd.to_datetime(arr.index)
-	if per_capita:
+	if per_capita or country2value:
 		n:int = 0
 		for _subregion in region_list:
+			if (country2value is not None) and (_subregion not in country2value):
+				continue
 			try:
 				n += get_population(_subregion)
 			except TypeError:
@@ -75,7 +82,7 @@ def graph_arr(_min:int, ax, arr, colour:str, name:str, per_capita:bool, region_l
 	plt.annotate(n_infected_now, xy=(plt.gca().get_xlim()[1],n_infected_now))
 
 
-def graph(name:str, df, df2, yscale:str, per_capita:bool, countries:list, regions:list, highlight:list, _min:int, split_regions:bool, daily_change:bool, is_total:bool):
+def graph(name:str, df, df2, yscale:str, per_capita:bool, country2value:dict, countries:list, regions:list, highlight:list, _min:int, split_regions:bool, daily_change:bool, is_total:bool, mark_dates:list):
 	drop_cols:list = ["Province/State","Country/Region","Lat","Long"]
 	fig, ax = plt.subplots()
 	plt.margins(x=0,y=0)
@@ -89,6 +96,8 @@ def graph(name:str, df, df2, yscale:str, per_capita:bool, countries:list, region
 	else:
 		_all_regions = all_regions
 	
+	df_mutable = df.copy()
+	
 	for region in regions:
 		_colour, _countries = _all_regions[region]
 		if split_regions:
@@ -101,7 +110,7 @@ def graph(name:str, df, df2, yscale:str, per_capita:bool, countries:list, region
 		if daily_change:
 			_df = to_daily_change(_df)
 		linestyle = "-" if region in highlight else "--"
-		graph_arr(_min, ax, _df.sum(axis=0), _colour, region, per_capita, _countries, 2, linestyle)
+		graph_arr(_min, ax, _df.sum(axis=0), _colour, region, per_capita, country2value, df_mutable, _countries, 2, linestyle)
 	
 	for country in countries:
 		_df = df.loc[df["Country/Region"]==country].drop(columns=drop_cols)
@@ -111,7 +120,19 @@ def graph(name:str, df, df2, yscale:str, per_capita:bool, countries:list, region
 		if daily_change:
 			_df = to_daily_change(_df)
 		linestyle = "-" if country in highlight else "--"
-		graph_arr(_min, ax, _df.sum(axis=0), None, country, per_capita, [country], 1, linestyle)
+		graph_arr(_min, ax, _df.sum(axis=0), None, country, per_capita, country2value, df_mutable, [country], 1, linestyle)
+	
+	for date in mark_dates:
+		label:str = date
+		if "=" in date:
+			date, label = date.split("=")
+		t:float = dt.strptime(date, "%Y-%m-%d")
+		ax.axvline(t, label=label)
+	
+	if country2value is not None:
+		_df = df.set_index("Country/Region").groupby(country2value).drop(columns=drop_cols).sum(axis=0)
+	
+	#_df.plot(ax=ax)
 	
 	plt.title(name + (" per day" if daily_change else ""))
 	ax.legend(loc="best")
@@ -120,12 +141,44 @@ def graph(name:str, df, df2, yscale:str, per_capita:bool, countries:list, region
 	plt.show()
 
 
-def get_tbl(s:str):
-	url:str = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_{which}_global.csv"
+def get_tbl__base(which:str):
+	filename:str = f"time_series_covid19_{which}_global.csv"
+	if os.path.isfile(filename) and os.path.getmtime(filename) > (dt.now().timestamp() - 3600*24):
+		return pd.read_csv(filename)
+	url:str = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/{filename}"
 	try:
-		return pd.read_csv(url)
+		print("Downloading latest dataset")
+		df = pd.read_csv(url)
+		df.to_csv(filename)
+		return df
 	except HTTPError as e:
 		raise Exception(f"{e}: {url}")
+
+
+def is_datetimestr_greater_than(a:str, t:float, default:bool):
+	try:
+		print(dt.strptime(a, "%m/%d/%y").timestamp(), t)
+		return dt.strptime(a, "%m/%d/%y").timestamp() >= t
+	except ValueError:
+		return default
+
+
+def get_tbl(which:str, _from:str, _to:str):
+	df = get_tbl__base(which)
+	
+	cols_to_keep:list = df.columns
+	if _from is not None:
+		cols_to_keep = [c for c in cols_to_keep if is_datetimestr_greater_than(c, dt.strptime(_from, "%Y-%m-%d").timestamp(), True)]
+	if _to is not None:
+		cols_to_keep = [c for c in cols_to_keep if not is_datetimestr_greater_than(c, dt.strptime(_to,   "%Y-%m-%d").timestamp(), False)]
+	
+	if _from or _to:
+		df = df[cols_to_keep]
+	
+	print(_from, _to)
+	print(df)
+	
+	return df
 
 
 def to_daily_change(df):
@@ -141,6 +194,9 @@ if __name__ == "__main__":
 	
 	parser = argparse.ArgumentParser()
 	parser.add_argument("which", nargs="*", help="confirmed deaths or recovered")
+	parser.add_argument("-a", help="YYYY-MM-DD")
+	parser.add_argument("-b", help="YYYY-MM-DD")
+	parser.add_argument("--mark-date", action="append", default=[], help="Mark dates on the graph")
 	parser.add_argument("--scale", default="log", help="log or linear or symlog or logit")
 	parser.add_argument("-r","--regions", nargs="*", help="Regions to graph. If none specified, all included. Options: '"+"' '".join(list(all_regions))+"'")
 	parser.add_argument("-R","--Regions", default=[], nargs="+", help="As --regions but highlighted")
@@ -154,22 +210,22 @@ if __name__ == "__main__":
 	parser.add_argument("-d","--daily-change", default=False, action="store_true")
 	parser.add_argument("--total", default=False, action="store_true")
 	parser.add_argument("--region-dir", default=False, action="store_true", help="Display the directory where regions are stored, and exit")
+	parser.add_argument("--country2value", help="Path of DSV file mapping country name to variable value. If supplied, will display a graph correlating SARS2 data with these values, per capita.")
 	args = parser.parse_args()
 	
 	if args.region_dir:
 		print(REGIONS_DIR)
 		exit(0)
 	
+	country2value:dict = None
+	if args.country2value is not None:
+		import re
+		country2value = {x:y for x,y in re.findall("([^\t]+)\t([^\n]+)", open(args.country2value).read())}
+	
 	verbosity:int = 1 + args.verbose - args.quiet
 	
 	args.countries = [] if args.countries is None else ["Mainland China","US","UK"] if len(args.countries)==0 else args.countries
 	args.regions = [] if args.regions is None else list(all_regions) if len(args.regions)==0 else args.regions
-	
-	from datetime import datetime as dt
-	import pandas as pd
-	import matplotlib.pyplot as plt
-	import matplotlib.dates as mdates
-	import numpy as np
 	
 	_printed_debug_reports:bool = False
 	
@@ -183,8 +239,8 @@ if __name__ == "__main__":
 		else:
 			which = _which
 		
-		df  = get_tbl(which)
-		df2 = get_tbl(which2) if which2 is not None else None
+		df  = get_tbl(which,  args.a, args.b)
+		df2 = get_tbl(which2, args.a, args.b) if which2 is not None else None
 		
 		if not _printed_debug_reports:
 			if verbosity > 0:
@@ -199,4 +255,4 @@ if __name__ == "__main__":
 						print(f"\t{country}")
 			_printed_debug_reports = True
 		
-		graph(_which, df, df2, args.scale, args.per_capita, args.countries+args.Countries, args.regions+args.Regions, args.Countries+args.Regions, args.min, args.split_regions, args.daily_change, args.total)
+		graph(_which, df, df2, args.scale, args.per_capita, country2value, args.countries+args.Countries, args.regions+args.Regions, args.Countries+args.Regions, args.min, args.split_regions, args.daily_change, args.total, args.mark_date)
